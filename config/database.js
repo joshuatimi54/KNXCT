17d3d4c6fffc982436985019449413ca6888853e
urizennnn <igamerryt@gmail.com>
2025-06-18 17:35:40 +0100

    chore: updating connection function

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const { MONGODB_URI } = require('./env');
 const connect = async () => {
   const uri = MONGODB_URI;
   try {
-    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(uri);
     console.log('Connected to MongoDB');
   } catch (err) {
     console.error('MongoDB connection error:', err);
