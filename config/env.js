fd889a1e0774f7d16b2a2efd52001b112ee14960
Joshua Adesoba <joshuatimi54@gmail.com>
2025-06-19 01:17:43 +0100

    Database connected

--- config/env.js
+++ config/env.js
@@ -5,6 +5,6 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || '3000',
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/mydatabase',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://joshuatimi54:Joshua21@knxct.ird4tcj.mongodb.net/KNXCT?retryWrites=true&w=majority&appName=KNXCT',
   CMS_API_KEY: process.env.CMS_API_KEY
 };
